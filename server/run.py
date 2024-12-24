6270f541611e74d57b0638e9a64d4bf17413e9eb
Jonah Carpenter <jonah4chiefs@gmail.com>
2024-12-24 06:46:04 -0600

    ripping my hair out because it wont run on my ubuntu unraid server because i dont have python installed globally

--- server/run.py
+++ server/run.py
@@ -5,4 +5,6 @@ app = create_app()
 
 if __name__ == "__main__":
     app_mode = os.environ.get('APP_MODE', 'production').lower()
-    app.run(host='localhost', port=5000, debug=(app_mode == 'development'))
+    host = os.environ.get('FLASK_HOST', 'localhost')
+    port = int(os.environ.get('FLASK_PORT', 5000))
+    app.run(host=host, port=port, debug=(app_mode == 'development'))
