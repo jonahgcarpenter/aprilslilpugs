912df2f15c8bf61de68420acb3cd7bb2994483d4
Jonah Carpenter <jonah4chiefs@gmail.com>
2024-12-29 00:24:23 -0600

    new

--- server/app/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Routes package initialization"""
-from .main import main_bp
-from .auth import auth_bp
-from .breeder import breeder_bp
-from .aboutus import aboutus_bp
-
-__all__ = ['main_bp', 'auth_bp', 'breeder_bp', 'aboutus_bp']
