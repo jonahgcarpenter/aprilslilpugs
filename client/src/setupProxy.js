1b5fbec8dde64486804c2acb1271d5b418ea6129
jonahgcarpenter <jonahgcarpenter@gmail.com>
2025-02-23 15:22:32 -0600

    proxy for stream

---
+++ client/src/setupProxy.js
@@ -0,0 +1,13 @@
+// FOR VIEWING THE LIVE STREAM ON A DEV SERVER
+// const { createProxyMiddleware } = require("http-proxy-middleware");
+//
+// module.exports = function (app) {
+//   app.use(
+//     "/hls",
+//     createProxyMiddleware({
+//       target: "https://aprilslilpugs.com",
+//       changeOrigin: true,
+//       secure: true,
+//     }),
+//   );
+// }
