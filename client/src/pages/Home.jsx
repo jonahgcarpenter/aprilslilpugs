e3a41a994b761485dfe995957a3a606302b1b123
jonahgcarpenter <jonahgcarpenter@gmail.com>
2025-04-04 14:47:07 -0500

    page now sends props

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import BreederInfo from "../components/Breeder/BreederInfo";
 
+const breeder = {
+  profilePicture: "src/assets/profilepic.png",
+  firstName: "April",
+  lastName: "Carpenter",
+  location: "Tupelo, MS",
+  email: "aprilslilpugs@gmail.com",
+  phoneNumber: "662-321-8352",
+  story:
+    "My name is April Carpenter.  I fell in love with pugs when I was 5.  At 7 I finally talked my parents into getting a pug. Sugar was my faithful companion until just before my 21st birthday.   \r\n\r\nI got married soon after that and my husband swore, we were never getting a pug.  20 years later I gave up and brought my baby Winston home.  It didn't take long before we were both in love.  Our love for pugs and the breed in general nudged us to start breeding quality companion pugs. ",
+  images: ["src/assets/gallery1.jpeg", "src/assets/gallery2.jpeg"],
+};
+
 const Home = () => {
   return (
     <div className="min-h-screen pt-8 pb-16">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 space-y-8 sm:space-y-12">
-        <BreederInfo />
+        <BreederInfo breeder={breeder} />
       </div>
     </div>
   );
