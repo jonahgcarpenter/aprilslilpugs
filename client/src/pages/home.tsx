6975a4a83cd4cd30705ec53044ec8dba9ddd01a8
jonahgcarpenter <jonahgcarpenter@gmail.com>
2025-12-06 12:56:50 -0600

    updated

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
   const { breeder, isLoading, error } = useBreeder();
 
   return (
-    <div className="min-h-screen pt-8 pb-16">
+    <div className="min-h-screen">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 space-y-8 sm:space-y-12">
         <AboutMe breeder={breeder} isLoading={isLoading} error={error} />
       </div>
