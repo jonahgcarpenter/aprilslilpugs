6f2861add61c43953b77951d59063d6746692e8e
jonahgcarpenter <jonahgcarpenter@gmail.com>
2025-12-13 10:56:50 -0600

    layout fix

--- client/src/data/pugs.ts
+++ client/src/data/pugs.ts
@@ -39,4 +39,44 @@ export const mockPugs: Pug[] = [
     image_ids: "img-pug-3",
     birth_date: "2021-03-10",
   },
+  {
+    id: "pug-4",
+    name: "Pug 1",
+    gender: "M",
+    description:
+      "Generic description for Pug 1. He is the main stud with a classic fawn coat and a temperament that is both calm and playful.",
+    profile_picture_id: "img-pug-1",
+    image_ids: "img-pug-1",
+    birth_date: "2019-05-15",
+  },
+  {
+    id: "pug-5",
+    name: "Pug 2",
+    gender: "F",
+    description:
+      "Generic description for Pug 2. She is an attentive and gentle mother who passes lovely traits to her puppies.",
+    profile_picture_id: "img-pug-2",
+    image_ids: "img-pug-2",
+    birth_date: "2020-11-20",
+  },
+  {
+    id: "pug-6",
+    name: "Pug 3",
+    gender: "F",
+    description:
+      "Generic description for Pug 3. She is full of energy, spunk, and has a very big personality.",
+    profile_picture_id: "img-pug-3",
+    image_ids: "img-pug-3",
+    birth_date: "2021-03-10",
+  },
+  {
+    id: "pug-7",
+    name: "Pug 1",
+    gender: "M",
+    description:
+      "Generic description for Pug 1. He is the main stud with a classic fawn coat and a temperament that is both calm and playful.",
+    profile_picture_id: "img-pug-1",
+    image_ids: "img-pug-1",
+    birth_date: "2019-05-15",
+  },
 ];
--- client/src/pages/our-adults.tsx
+++ client/src/pages/our-adults.tsx
@@ -4,13 +4,7 @@ import { usePug } from "../hooks/usepugs";
 const OurAdults = () => {
   const { pugs, isLoading, error } = usePug();
 
-  return (
-    <div className="min-h-screen">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 space-y-8 sm:space-y-12">
-        <Grumble grumbles={pugs} isLoading={isLoading} error={error} />
-      </div>
-    </div>
-  );
+  return <Grumble grumbles={pugs} isLoading={isLoading} error={error} />;
 };
 
 export default OurAdults;
