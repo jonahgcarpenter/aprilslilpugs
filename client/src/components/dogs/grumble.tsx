cdc8a13da84805a0b2eed999f48f118e335c5d5d
jonahgcarpenter <jonahgcarpenter@gmail.com>
2025-12-22 17:03:24 -0600

    remove transform

--- client/src/components/dogs/grumble.tsx
+++ client/src/components/dogs/grumble.tsx
@@ -72,7 +72,7 @@ const Grumble = ({ grumbles, isLoading, error }: GrumbleProps) => {
                       <img
                         src={dog.profilePicture}
                         alt={dog.name}
-                        className="w-full h-full object-cover transform hover:scale-105 transition-transform duration-500"
+                        className="w-full h-full object-cover"
                       />
                     ) : (
                       <div className="w-full h-full bg-slate-800 flex items-center justify-center text-slate-600">
